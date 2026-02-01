f1692499e32be9676deed6aacad59c9d06a7aa21
AdityaSinghBynd <aditya@bynd.ai>
2026-02-01 20:43:13 +0530

    Certifications hide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <Skills />
         <Experience />
         {/* <Projects /> */}
-        <Certifications />
+        {/* <Certifications /> */}
         <Blogs />
         <Contacts />
       </div>
